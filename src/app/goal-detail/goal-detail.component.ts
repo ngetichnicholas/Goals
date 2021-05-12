0dca1ece56c753e2b52a0be051e87c3191056308
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-12 15:33:39 +0300

    Add date pipe

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Goal } from '../goal';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal-detail',
@@ -9,9 +8,15 @@ import { Goal } from '../goal';
 export class GoalDetailComponent implements OnInit {
 
   @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
