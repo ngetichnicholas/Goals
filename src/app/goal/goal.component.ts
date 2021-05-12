b3c1ec90b9900e6be39b1b7628b10c7a335c1470
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-12 21:58:50 +0300

    Update app and add bootstrap

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -17,20 +17,25 @@ export class GoalComponent implements OnInit {
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
 
-    toggleDetails(index){
-      this.goals[index].showDescription = !this.goals[index].showDescription;
-    }
-  
-    completeGoal(isComplete, index){
-      if (isComplete) {
-        this.goals.splice(index,1);
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  deleteGoal(isComplete, index){
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
       }
     }
-    
-    constructor() { }
-   
-    ngOnInit() {
-    }
+  }
+
+  
+  constructor() { }
+  
+  ngOnInit() {
+  }
   
 }
   
