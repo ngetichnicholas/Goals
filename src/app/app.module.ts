1af47033ed68e2b14966b650319f5cab04140f65
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-05-18 14:37:13 +0300

    Add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoalService } from './goal-service/goal.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,9 +24,10 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [GoalService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,46 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { AlertService } from '../alert-service/alert.service';
+
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css'],
+  providers: [GoalService]
+
 })
 export class GoalComponent implements OnInit {
-  goals: Goal[] = [
-    new Goal(
-      1,
-      'Watch finding Nemo',
-      'Find an online version and watch merlin find his son',
-      new Date(2019, 9, 14)
-    ),
-    new Goal(
-      2,
-      'Buy Cookies',
-      'I have to buy cookies for the parrot',
-      new Date(2019, 6, 9)
-    ),
-    new Goal(
-      3,
-      'Get new Phone Case',
-      'Diana has her birthday coming up soon',
-      new Date(2019, 1, 12)
-    ),
-    new Goal(
-      4,
-      'Get Dog Food',
-      'Pupper likes expensive snacks',
-      new Date(2019, 11, 18)
-    ),
-    new Goal(5, 'Solve math homework', 'Damn Math', new Date(2019, 2, 14)),
-    new Goal(
-      6,
-      'Plot my world domination plan',
-      'Cause I am an evil overlord',
-      new Date(2019, 3, 14)
-    ),
-  ];
+  goals:Goal[];
+  alertService:AlertService;
 
+  constructor(goalService:GoalService, alertService:AlertService) {
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
+  }
+  
   toggleDetails(index) {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
@@ -68,8 +47,6 @@ export class GoalComponent implements OnInit {
       }
     }
   }
-
-  constructor() {}
 
   ngOnInit() {}
 }
